9db9c739f8069c31ba74408865d6848223ce322c
lingFun <49457941+lingFun@users.noreply.github.com>
2020-09-21 12:22:48 -0400

    added contactInfo table and changed to php send_email

---
+++ functions/handle_contact.php
@@ -0,0 +1,37 @@
+<?php
+    require_once('../functions/config.php');
+
+    //Handle contact form info and send email 
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $companyName = clean($_POST['companyName']);
+        $organizationType = clean($_POST['organizationType']);
+        $name = clean($_POST['name']);
+        $phoneNumber = clean($_POST['phoneNumber']);
+        $emailAddress = clean($_POST['emailAddress']);
+        $message = clean($_POST['message']);
+
+        $sql ="INSERT INTO contactInfo (CompanyName, OrganizationType, FullName, PhoneNumber, Email) 
+            VALUES ('$companyName' ,'$organizationType', '$name', '$phoneNumber', '$emailAddress')";
+        $result = Query($sql);
+        confirm($result);
+        // $rs = mysqli_query($con, $sql);
+        // if($rs)
+        // {
+        //     echo "Contact Records Inserted";
+        // }
+
+        $mailTo = "lfang001@citymail.cuny.edu";
+        $headers = "From: life3.io"  . "\r\n" .
+        "CC: omar.duran@life3.io";
+        $txt = "Company name: ".$companyName."\nOrganization type: ".$organizationType."\nName: ".$name.
+        "\nEmail address: ".$emailAddress."\nPhone#: ".$phoneNumber."\nMessage: ".$message;
+
+
+        mail($mailTo, $companyName, $txt, $headers);
+        // header("Location: ../thankyou.html");
+        redirect("../Pages/thankyou.html");
+    }
+
+
+?>
